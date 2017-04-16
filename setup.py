48d77c04a1bf65e2f6062b55fd33eee035933269
Albertas Agejevas <alga@pov.lt>
2017-04-16 22:16:58 +0300

    Boilerplate.

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name="clipper",
+    version="0.1dev0",
+    author="Albertas Agejevas",
+    author_email="albertas.agejevas@gmail.com",
+    description="Automatically cut videos out of footage",
+    license="MIT",
+    packages=['clipper'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
